1786b101d1790a67713e2a779fa7fb1d4edacb85
Praveen Dias <praveendias1180@gmail.com>
2022-07-31 15:13:52 +0530

    Simple rectangle

--- canvas.js
+++ canvas.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'green';
+ctx.fillStyle = 'rgba(0,0,255,0.5)';
 ctx.fillRect(10, 10, 150, 100);
