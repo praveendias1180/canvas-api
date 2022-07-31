35e95210c7b32e7aa9e5a882f0c32cb7eb2e4fa3
Praveen Dias <praveendias1180@gmail.com>
2022-07-31 15:15:39 +0530

    Overlapping rectangles

--- canvas.js
+++ canvas.js
@@ -1,5 +1,8 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('tutorial');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'rgba(0,0,255,0.5)';
-ctx.fillRect(10, 10, 150, 100);
+ctx.fillStyle = 'rgb(200, 0, 0)';
+ctx.fillRect(10, 10, 50, 50);
+
+ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+ctx.fillRect(30, 30, 50, 50);
