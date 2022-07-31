e7256b751b1717f03362481811cf840b5fab7f59
Praveen Dias <praveendias1180@gmail.com>
2022-07-31 15:37:13 +0530

    Smiley Face

--- canvas.js
+++ canvas.js
@@ -18,3 +18,23 @@ function drawBoxes() {
 function clearCanvas() {
   ctx.clearRect(0, 0, 150, 150);
 }
+
+function makeArrow(){
+    ctx.beginPath();
+    ctx.moveTo(75, 50);
+    ctx.lineTo(100, 75);
+    ctx.lineTo(100, 25);
+    ctx.fill();    
+}
+
+function smileyFace(){
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+    ctx.stroke();
+}
