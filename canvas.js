885df74b6c343e4344795040eff5b8349814f7c6
Praveen Dias <praveendias1180@gmail.com>
2022-07-31 16:04:22 +0530

    Checkboxes

--- canvas.js
+++ canvas.js
@@ -132,3 +132,12 @@ function roundedRect(ctx, x, y, width, height, radius) {
   ctx.arcTo(x, y, x, y + radius, radius);
   ctx.stroke();
 }
+
+function checkBox(){
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 6; j++) {
+          ctx.fillStyle = `rgb(${Math.floor(255 - 42.5 * i)}, ${Math.floor(255 - 42.5 * j)}, 0)`;
+          ctx.fillRect(j * 25, i * 25, 25, 25);
+        }
+      }    
+}
